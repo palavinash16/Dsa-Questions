58b56d621e2182880139c8421240c26a3c5e8529
Avinash Kumar Pal <palavinashkumar6@gmail.com>
2026-03-13 14:43:27 +0530

    Time: 3 ms (30.45%) | Memory: 9.9 MB (77.49%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+bool checkPalindrome(string &s, int left, int right) {
+    
+    // Base condition
+    if(left >= right)
+        return true;
+
+    // Skip non-alphanumeric characters
+    if(!isalnum(s[left]))
+        return checkPalindrome(s, left + 1, right);
+
+    if(!isalnum(s[right]))
+        return checkPalindrome(s, left, right - 1);
+
+    // Compare characters
+    if(tolower(s[left]) != tolower(s[right]))
+        return false;
+
+    // Recursive call
+    return checkPalindrome(s, left + 1, right - 1);
+}
+
+    bool isPalindrome(string s) {
+        return checkPalindrome(s, 0, s.length() - 1);
+
+        
+    }
+};
