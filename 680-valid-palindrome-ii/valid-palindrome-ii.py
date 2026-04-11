5fc66933bdc2d709b9048631cd17669e5a67934d
Avinash Kumar Pal <palavinashkumar6@gmail.com>
2026-04-12 00:24:28 +0530

    Time: 43 ms (90.85%) | Memory: 12.6 MB (52.33%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -7,7 +7,7 @@ class Solution(object):
         left=0
         right=len(s)-1
 
-        while left<right and len(s)>1:
+        while left<right:
             if s[left]!=s[right]:
                 s1 = s[left+1:right+1]
                 s2 = s[left:right]
