155f492789e4dd87b824dc7a9d20dbbefbf03b4a
Avinash Kumar Pal <palavinashkumar6@gmail.com>
2026-04-11 23:57:06 +0530

    Time: 3 ms (54.83%) | Memory: 19.9 MB (53.79%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -4,13 +4,14 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
+        left=0
         right=len(s)-1
 
-        for left in range(0,len(s)):
-            if left <right:
-                s[right],s[left]=s[left],s[right]
-                left+=1
-                right-=1
+        
+        while left <right:
+            s[right],s[left]=s[left],s[right]
+            left+=1
+            right-=1
 
         return s
 
